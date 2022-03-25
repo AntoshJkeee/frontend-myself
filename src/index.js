af29c07c59947ec765ba11db53c1877b925e5364
AntoshJkeee <toha_bart@mail.ru>
2022-03-25 15:13:15 +0300

    test1231377

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles/index.scss';
 // let a = 123
 
 console.log('test' + a);console.log('test' + a);
+
 console.log('test' + a);console.log('test' + a);
 // console.log('test' + a);console.log('test' + a);
 // console.log('test' + a);console.log('test' + a);
