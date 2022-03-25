9dec6c79f7a2299b6e59446e8a2b6e88e1bc7d45
Sevostyanov Evgeniy <sevostyanov@aicrobotics.ru>
2022-03-25 14:00:30 +0300

    окончательная настройка eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import './styles/index.scss';
 
 
 
-let a = 123
+// let a = 123
 
-console.log('test' + a);console.log('test' + a);
-console.log('test' + a);console.log('test' + a);
+// console.log('test' + a);console.log('test' + a);
+// console.log('test' + a);console.log('test' + a);
+// console.log('test' + a);console.log('test' + a);
+// console.log('test' + a);console.log('test' + a);
