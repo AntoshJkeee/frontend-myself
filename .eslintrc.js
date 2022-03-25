a60d2257c255ad49124ae53285bc622e82f582b5
AntoshJkeee <toha_bart@mail.ru>
2022-03-25 09:58:20 +0300

    asd new 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   "parser": "@babel/eslint-parser",
   "rules": {
     "no-unused-vars": "warn",
-    "no-console": "warn",
+    "no-console": "error",
     "semi": "warn",
     "no-var": "warn"
   },
