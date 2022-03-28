429469ed83102e43fdff59502f615ed5fee47b1f
Sevostyanov Evgeniy <sevostyanov@aicrobotics.ru>
2022-03-28 09:29:05 +0300

    test husky

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ let b = 123
 console.log('test' + a);console.log('test' + a);
 console.log('test' + a);console.log('test' + a);
 console.log('test' + a);console.log('test' + a);
-console.log('test' + a);console.log('test' + a);console.log('test' + a);console.log('test' + a);
-console.log('test' + a);console.log('test' + a);
+console.log('test' + a);console.log('test' + a)
 
 // console.log('test' + a);console.log('test' + a);
